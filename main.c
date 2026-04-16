e77c2cec0de4d2914d33900d20d8c3ed9cbd2fdc
dropboy27 <pyanzinmihail@gmail.com>
2026-04-16 22:34:30 +0300

    final

--- main.c
+++ main.c
@@ -10,26 +10,25 @@ typedef enum {
     NODE_UNARY_MINUS
 } NodeType;
 
-struct Node {
+typedef struct Node {
     NodeType type;
-    int num_value;      // для NODE_NUMBER
-    char var_name;      // для NODE_VARIABLE
-    char op;            // для NODE_BINARY_OP: '+', '-', '*', '/'
+    int num_value;
+    char var_name;
+    char op;
     struct Node *left;
     struct Node *right;
-};
-typedef struct Node Node;
+}Node;
 
 typedef struct {
     Node* node;
-    int odd;   // 1 если нечётное количество минусов
+    int odd;   //если нечётное количество минусов
 } SimplifyResult;
 
 
 typedef enum {
     TOKEN_NUMBER,
     TOKEN_VARIABLE,
-    TOKEN_OPERATOR,      // бинарные + - * /
+    TOKEN_OPERATOR,      // бинарные
     TOKEN_UNARY_MINUS,   // унарный минус
     TOKEN_LPAREN,
     TOKEN_RPAREN,
@@ -46,7 +45,7 @@ typedef struct {
 Token tokens[MAX_TOKENS];
 int token_count = 0;
 
-int is_unary_context(Token tokens[], int count) {
+int is_unary(Token tokens[], int count) {
     if (count == 0) return 1;
     TokenType last = tokens[count-1].type;
     return (last == TOKEN_LPAREN || last == TOKEN_OPERATOR);
@@ -82,7 +81,7 @@ void parse_expression(const char *str) {
 
         if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
             if (str[i] == '-') {
-                if (is_unary_context(tokens, token_count)) {
+                if (is_unary(tokens, token_count)) {
                     tokens[token_count].type = TOKEN_UNARY_MINUS;
                     token_count++;
                     i++;
@@ -154,142 +153,291 @@ Node* create_unary_minus(Node* operand) {
     return node;
 }
 
-Node* build_tree(Token tokens[], int token_count) {
+Node* build_tree(Token *tokens, int length){
     Node* operand_stack[MAX_TOKENS];
-    int operand_top = -1;   // вершина стека операндов
-
+    int operand_stack_len = -1;
     struct {
         char op;
         int is_unary;
     } operator_stack[MAX_TOKENS];
-    int operator_top = -1;   // вершина стека операторов
-
-    for (int i = 0; i < token_count && tokens[i].type != TOKEN_END; i++) {
-        if (tokens[i].type == TOKEN_NUMBER) {
-            operand_stack[++operand_top] = create_number_node(tokens[i].int_value);
+    int operator_stack_len = -1;
+    for (int i = 0; i < length; i++){
+        Token current_element = tokens[i];
+        if (current_element.type == TOKEN_NUMBER){
+            Node *New_Node = create_number_node(current_element.int_value);
+            operand_stack[++operand_stack_len] = New_Node;
+            continue;
         }
-        else if (tokens[i].type == TOKEN_VARIABLE) {
-            operand_stack[++operand_top] = create_variable_node(tokens[i].var_name);
+        else if (current_element.type == TOKEN_VARIABLE){
+            Node *New_Node = create_variable_node(current_element.var_name);
+            operand_stack[++operand_stack_len] = New_Node;
+            continue;
         }
-        else if (tokens[i].type == TOKEN_UNARY_MINUS) {
-            // Кладём унарный минус в стек операторов
-            operator_stack[++operator_top].op = '-';
-            operator_stack[operator_top].is_unary = 1;
+        else if (current_element.type == TOKEN_UNARY_MINUS){
+            operator_stack[++operator_stack_len].op = '-';
+            operator_stack[operator_stack_len].is_unary = 1;
+            continue;
         }
-        else if (tokens[i].type == TOKEN_OPERATOR) {
-            char op = tokens[i].op;
-            int prec = priority(op, 0);  // бинарный оператор
-            // Выталкиваем операторы с большим или равным приоритетом
-            while (operator_top >= 0 && operator_stack[operator_top].op != '(' &&
-                   priority(operator_stack[operator_top].op, operator_stack[operator_top].is_unary) >= prec) {
-                // Выталкиваем верхний оператор
-                char top_op = operator_stack[operator_top].op;
-                int is_unary = operator_stack[operator_top].is_unary;
-                operator_top--;
-
+        else if (current_element.type == TOKEN_OPERATOR){
+            while (operator_stack_len >= 0
+                            && priority(operator_stack[operator_stack_len].op,
+                            operator_stack[operator_stack_len].is_unary)>=priority(current_element.op, 0)
+                            && operator_stack[operator_stack_len].op!='(')
+            {
+                char op = operator_stack[operator_stack_len].op;
+                int is_unary = operator_stack[operator_stack_len].is_unary;
+                operator_stack_len--;
                 if (is_unary) {
-                    // Унарный оператор: берём один операнд
-                    Node* operand = operand_stack[operand_top--];
-                    operand_stack[++operand_top] = create_unary_minus(operand);
+                    Node* operand = operand_stack[operand_stack_len];
+                    operand_stack[operand_stack_len] = create_unary_minus(operand);
                 } else {
-                    // Бинарный оператор: берём два операнда
-                    Node* right = operand_stack[operand_top--];
-                    Node* left  = operand_stack[operand_top--];
-                    operand_stack[++operand_top] = create_binary_op(top_op, left, right);
+                    Node* right = operand_stack[operand_stack_len--];
+                    Node* left  = operand_stack[operand_stack_len];
+                    Node* new_node = create_binary_op(op, left, right);
+                    operand_stack[operand_stack_len] = new_node;
                 }
             }
-            // Кладём текущий оператор в стек
-            operator_stack[++operator_top].op = op;
-            operator_stack[operator_top].is_unary = 0;
+            operator_stack[++operator_stack_len].op = current_element.op;
+            operator_stack[operator_stack_len].is_unary = 0;
+            continue;
         }
-        else if (tokens[i].type == TOKEN_LPAREN) {
-            operator_stack[++operator_top].op = '(';
-            operator_stack[operator_top].is_unary = 0;
+        else if (current_element.type == TOKEN_LPAREN){
+            operator_stack[++operator_stack_len].op = '(';
+            operator_stack[operator_stack_len].is_unary = 0;
+            continue;
         }
-        else if (tokens[i].type == TOKEN_RPAREN) {
-            // Выталкиваем все операторы до открывающей скобки
-            while (operator_top >= 0 && operator_stack[operator_top].op != '(') {
-                char top_op = operator_stack[operator_top].op;
-                int is_unary = operator_stack[operator_top].is_unary;
-                operator_top--;
-
+        else if (current_element.type == TOKEN_RPAREN){
+            while (operator_stack_len >= 0 && operator_stack[operator_stack_len].op != '('){
+                char op = operator_stack[operator_stack_len].op;
+                int is_unary = operator_stack[operator_stack_len].is_unary;
+                operator_stack_len--;
                 if (is_unary) {
-                    Node* operand = operand_stack[operand_top--];
-                    operand_stack[++operand_top] = create_unary_minus(operand);
+                    Node* operand = operand_stack[operand_stack_len];
+                    operand_stack[operand_stack_len] = create_unary_minus(operand);
                 } else {
-                    Node* right = operand_stack[operand_top--];
-                    Node* left  = operand_stack[operand_top--];
-                    operand_stack[++operand_top] = create_binary_op(top_op, left, right);
+                    Node* right = operand_stack[operand_stack_len--];
+                    Node* left  = operand_stack[operand_stack_len];
+                    Node* new_node = create_binary_op(op, left, right);
+                    operand_stack[operand_stack_len] = new_node;
                 }
             }
-            // Убираем открывающую скобку
-            if (operator_top >= 0 && operator_stack[operator_top].op == '(')
-                operator_top--;
+            if (operator_stack_len >= 0 && operator_stack[operator_stack_len].op == '(')
+                operator_stack_len--;
+            continue;
         }
     }
-
-    // После обработки всех токенов выталкиваем оставшиеся операторы
-    while (operator_top >= 0) {
-        char top_op = operator_stack[operator_top].op;
-        int is_unary = operator_stack[operator_top].is_unary;
-        operator_top--;
-
+    while (operator_stack_len>=0){
+        char op = operator_stack[operator_stack_len].op;
+        int is_unary = operator_stack[operator_stack_len].is_unary;
+        operator_stack_len--;
         if (is_unary) {
-            Node* operand = operand_stack[operand_top--];
-            operand_stack[++operand_top] = create_unary_minus(operand);
+            Node* operand = operand_stack[operand_stack_len];
+            operand_stack[operand_stack_len] = create_unary_minus(operand);
         } else {
-            Node* right = operand_stack[operand_top--];
-            Node* left  = operand_stack[operand_top--];
-            operand_stack[++operand_top] = create_binary_op(top_op, left, right);
+            Node* right = operand_stack[operand_stack_len--];
+            Node* left  = operand_stack[operand_stack_len];
+            Node* new_node = create_binary_op(op, left, right);
+            operand_stack[operand_stack_len] = new_node;
         }
     }
-
-    // В стеке операндов должен остаться один узел – корень дерева
-    if (operand_top == 0)
+    if (operand_stack_len == 0) {
         return operand_stack[0];
-    else
-        return NULL; // ошибка: выражение некорректно
+    } else {
+        // Ошибка: в стеке операндов не один элемент
+        printf("Ошибка: некорректное выражение\n");
+        return NULL;
+    }
 }
 
+
+void print_tree_vertical(Node* node, int level) {
+    if (!node) return;
+
+    // Сначала печатаем правого потомка
+    if (node->type == NODE_BINARY_OP || node->type == NODE_UNARY_MINUS) {
+        print_tree_vertical(node->right, level + 1);
+    }
+
+    // Отступы
+    for (int i = 0; i < level; i++)
+        printf("    ");
+
+    // Печатаем текущий узел
+    if (node->type == NODE_NUMBER)
+        printf("%d\n", node->num_value);
+    else if (node->type == NODE_VARIABLE)
+        printf("%c\n", node->var_name);
+    else if (node->type == NODE_BINARY_OP)
+        printf("%c\n", node->op);
+    else if (node->type == NODE_UNARY_MINUS)
+        // можно вывести как "-" или "(-)"
+        printf("-\n");
+
+    // Затем левого потомка (будет ниже)
+    if (node->type == NODE_BINARY_OP) {
+        print_tree_vertical(node->left, level + 1);
+    }
+}
+
+
+void print_infix(Node* node) {
+    if (!node) return;
+
+    if (node->type == NODE_NUMBER){
+        printf("%d", node->num_value);
+    }
+    else if (node->type == NODE_VARIABLE){
+        printf("%c", node->var_name);
+    }
+    else if (node->type == NODE_UNARY_MINUS){
+        printf("-");
+        if (node->right->type != NODE_NUMBER && node->right->type != NODE_VARIABLE){
+            printf("(");
+            print_infix(node->right);
+            printf(")");
+        }
+        else{
+            print_infix(node->right);
+        }
+    }
+    else if (node->type == NODE_BINARY_OP){
+        if (node->left->type == NODE_BINARY_OP){
+            if (priority(node->op, 0)>priority(node->left->op, 0)){
+                printf("(");
+                print_infix(node->left);
+                printf(")");
+            }
+            else {
+                print_infix(node->left);
+            }
+        }
+        else{
+            print_infix(node->left);
+        }
+        printf(" %c ", node->op);
+        if (node->right->type == NODE_BINARY_OP){
+            if (priority(node->op, 0)>priority(node->right->op, 0) ||
+            (priority(node->op, 0) == priority(node->right->op, 0) &&
+            (node->right->op == '/' || node->right->op == '-'))){
+                printf("(");
+                print_infix(node->right);
+                printf(")");
+                }
+            else {
+                print_infix(node->right);
+            }
+        }
+        else if (node->right->type == NODE_UNARY_MINUS && (node->op == '-' || node->op == '/')){
+            printf("(");
+            print_infix(node->right);
+            printf(")");
+        }
+        else{
+            print_infix(node->right);
+        }
+    }
+}
+
+Node* copy_node(Node* src) {
+    Node* dst = (Node*)malloc(sizeof(Node));
+    dst->type = src->type;
+    if (src->type == NODE_NUMBER){
+        dst->num_value = src->num_value;
+    }
+    else if (src->type == NODE_VARIABLE){
+        dst->var_name = src->var_name;
+    }
+    dst->left = dst->right = NULL;
+    return dst;
+}
+
+// Рекурсивное упрощение (вынос унарных минусов)
+SimplifyResult extract_minuses(Node* node) {
+    SimplifyResult res;
+    if (!node){
+        res.node = NULL;
+        res.odd = 0;
+        return res;
+    }
+
+    if (node->type == NODE_NUMBER || node->type == NODE_VARIABLE) {
+        res.node = copy_node(node);
+        res.odd = 0;
+        return res;
+    }
+    if (node->type == NODE_UNARY_MINUS) {
+        SimplifyResult child = extract_minuses(node->right);
+        res.node = child.node;
+        res.odd = 1 - child.odd;
+        return res;
+    }
+    if (node->type == NODE_BINARY_OP && node->op == '*') {
+        SimplifyResult left_res = extract_minuses(node->left);
+        SimplifyResult right_res = extract_minuses(node->right);
+        int total_odd = (left_res.odd + right_res.odd) % 2;
+        Node* prod = create_binary_op('*', left_res.node, right_res.node);
+        res.node = prod;
+        res.odd = total_odd;
+        return res;
+    }
+    // Для других операций обходим, минусы не выносим
+    SimplifyResult left_res = extract_minuses(node->left);
+    SimplifyResult right_res = extract_minuses(node->right);
+    res.node = create_binary_op(node->op, left_res.node, right_res.node);
+    res.odd = 0;
+    return res;
+}
+
+Node* simplify(Node* root) {
+    SimplifyResult res = extract_minuses(root);
+    if (res.odd == 1)
+        return create_unary_minus(res.node);
+    else
+        return res.node;
+}
+
+// Освобождение памяти дерева
+void free_tree(Node* node) {
+    if (!node) return;
+    if (node->type == NODE_BINARY_OP) {
+        free_tree(node->left);
+        free_tree(node->right);
+    } else if (node->type == NODE_UNARY_MINUS) {
+        free_tree(node->right);
+    }
+    free(node);
+}
 
 int main() {
     char input[256];
-    printf("Введите арифметическое выражение: ");
+    printf("Enter expression: ");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
 
+    // 1. Исходное выражение в текстовом виде
+    printf("Original expression: %s\n", input);
+
     parse_expression(input);
-
-    // Вывод токенов (опционально, для отладки)
-    printf("\nТокены: ");
-    for (int i = 0; i < token_count; i++) {
-        switch (tokens[i].type) {
-            case TOKEN_NUMBER: printf("NUM(%d) ", tokens[i].int_value); break;
-            case TOKEN_VARIABLE: printf("VAR(%c) ", tokens[i].var_name); break;
-            case TOKEN_OPERATOR: printf("OP(%c) ", tokens[i].op); break;
-            case TOKEN_UNARY_MINUS: printf("UNARY_MINUS "); break;
-            case TOKEN_LPAREN: printf("LP "); break;
-            case TOKEN_RPAREN: printf("RP "); break;
-            default: break;
-        }
-    }
-    printf("\n");
-
     Node* root = build_tree(tokens, token_count);
-    printf("\nИсходное выражение (текст): ");
-    print_infix(root);
-    printf("\nИсходное дерево:\n");
-    print_tree_debug(root, 0);
+    if (!root) {
+        printf("Error building tree\n");
+        return 1;
+    }
 
     Node* simplified = simplify(root);
-    printf("\nПреобразованное выражение (текст): ");
-    print_infix(simplified);
-    printf("\nПреобразованное дерево:\n");
-    print_tree_debug(simplified, 0);
 
-    // Освобождение памяти
+    // 2. Преобразованное дерево
+    printf("\nTransformated tree:\n");
+    print_tree_vertical(simplified, 0);
+
+    // 3. Преобразованное выражение в текстовом виде
+    printf("\nTransformated expression: ");
+    print_infix(simplified);
+    printf("\n");
+
     free_tree(root);
     free_tree(simplified);
-
     return 0;
 }
+
+
